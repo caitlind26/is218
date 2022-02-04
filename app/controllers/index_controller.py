32626d883f55b298d068c66a98b8b3ad63ce2cee
Caitlin D'Souza <cd394@njit.edu>
2022-02-03 19:54:40 -0500

    changed name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Caitlin"
+        name = "Caitlin D'Souza"
         return render_template('index.html', name=name)
